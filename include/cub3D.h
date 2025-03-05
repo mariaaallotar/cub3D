6f096aeed5d890ab69425f803bcfdad3839c8aaf
lemercie <lemercie@student.hive.fi>
2025-03-05 12:29:55 +0200

    update header

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:04:23 by lemercie          #+#    #+#             */
-/*   Updated: 2025/03/05 10:53:46 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/03/05 12:25:22 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@
 # include "../lib/libft/libft.h"
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 # include <math.h>
+
+//validate_args.c
+void	validate_arguments(int argc, char **argv);
 
 #endif
